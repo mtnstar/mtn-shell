35c97323694d654ee151b57480eb2ab10b8bebb0
Mountain Star <github@mtnstar.net>
2025-11-03 09:35:35 +0100

    Use host podman from inside container

--- source.sh
+++ source.sh
@@ -2,16 +2,20 @@
 
 mtn() {
   mkdir -p ~/.mtn
+  systemctl --user start podman.socket
 
   podman run --rm -it \
     --pull always \
-    --privileged \
+    -v /var/run/user/$(id -u)/podman/podman.sock:/var/run/user/1000/podman/podman.sock \
     -v ~/.mtn:/home/mtn-admin \
-    --tmpfs /run --tmpfs /tmp \
-    -v /etc/subuid:/etc/subuid:ro \
-    -v /etc/subgid:/etc/subgid:ro \
-    -v /usr/bin/newuidmap:/usr/bin/newuidmap:ro \
-    -v /usr/bin/newgidmap:/usr/bin/newgidmap:ro \
+    --tmpfs /tmp \
     --userns=keep-id:uid=$(id -u),gid=$(id -g) \
+    -e CONTAINER_HOST=unix:///var/run/user/1000/podman/podman.sock \
     ghcr.io/mtnstar/mtn-shell:latest bash
 }
+    #-v /etc/subuid:/etc/subuid:ro \
+    #-v /etc/subgid:/etc/subgid:ro \
+    #-v /usr/bin/newuidmap:/usr/bin/newuidmap:ro \
+    #-v /usr/bin/newgidmap:/usr/bin/newgidmap:ro \
+    #--privileged \
+    #--security-opt label=disable \

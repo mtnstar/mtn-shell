f156a94d5f513a5dfefc29fad16e0391d731bc5e
Mountain Star <github@mtnstar.net>
2025-11-11 19:40:19 +0100

    Fix pull flag, add cap net raw to make ping possible from inside container

--- source.sh
+++ source.sh
@@ -16,9 +16,9 @@ mtn() {
   systemctl --user start podman.socket
 
   podman run --rm -it ${pull_flag} \
-    --pull always \
     -v /var/run/user/$(id -u)/podman/podman.sock:/var/run/user/1000/podman/podman.sock \
     -v ~/.mtn:/home/mtn-admin \
+    --cap-add=NET_RAW \
     --tmpfs /tmp \
     --userns=keep-id:uid=$(id -u),gid=$(id -g) \
     ghcr.io/mtnstar/mtn-shell:latest bash

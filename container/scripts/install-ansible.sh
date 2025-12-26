360581cb7369666c116b3632843916d661e7c62d
Mountain Star <github@mtnstar.net>
2025-12-26 18:55:13 +0000

    Update ansible, molecule, add kubernetes pip

--- container/scripts/install-ansible.sh
+++ container/scripts/install-ansible.sh
@@ -3,9 +3,10 @@ set -euo pipefail
 
 python3 -m venv /opt/python-venv
 pip install --no-cache-dir --upgrade pip==24.0 \
-  molecule==25.11.0 \
-  ansible==12.2.0 \
+  molecule==25.12.0 \
+  ansible==13.1.0 \
+  kubernetes==34.1.0 \
   pytest \
   testinfra \
   yamllint \
-  "molecule-plugins[docker]==25.8.12"
+  "molecule-plugins[podman]==25.8.12"
